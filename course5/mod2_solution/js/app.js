3b4eccfddeaa6596f6380e1a8f22597546b04263
dspicer5 <80365857+dspicer5@users.noreply.github.com>
2022-03-12 16:07:13 -0500

    input boxes

--- course5/mod2_solution/js/app.js
+++ course5/mod2_solution/js/app.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-
+    // ng-app
     angular.module('ShoppingListApp', [])
         .controller('ToBuyController', ToBuyController)
         .controller('AlreadyBoughtController', AlreadyBoughtController)
@@ -14,7 +14,7 @@
         TBC.array = ShoppingListCheckOffService.getTB();
         TBC.emptyArray = ShoppingListCheckOffService.emptyTB;
         TBC.moveItem = ShoppingListCheckOffService.moveItem;
-    }
+    } // end controller
 
     // controller 2 "" as ABC
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
@@ -23,29 +23,26 @@
         // getter function for AB array
         ABC.array = ShoppingListCheckOffService.getAB();
         ABC.emptyArray = ShoppingListCheckOffService.emptyAB;
-    }
+    } // end controller
 
     // connecting service
     function ShoppingListCheckOffService() {
         var service = this;
-
-        // define to buy "TB"
+        // define to buy "TB" array
         service.toBuy = [
-            {name: "Milk", quantity: 2, pricePerItem: 4},
-            {name: "Donuts", quantity: 200, pricePerItem: 3},
-            { name: "Cookies",  quantity: 100, pricePerItem: 1},
-            { name: "Coke", quantity: 12, pricePerItem: 1 },
-            { name: "Chicken", quantity: 6, pricePerItem: 2 },
-            { name: "Fries", quantity: 3, pricePerItem: 3 },
-            { name: "Sushi", quantity: 2, pricePerItem: 2 },
-            { name: "Steak", quantity: 1, pricePerItem: 20 },
-            { name: "Pork", quantity: 1, pricePerItem: 10 },
-            { name: "Bacon",  quantity: 5, pricePerItem: 3}
+            {name: "Milk", quantity: 2, price: 4},
+            {name: "Donuts", quantity: 20, price: 3},
+            { name: "Cookies",  quantity: 10, price: 1},
+            { name: "Coke", quantity: 12, price: 1 },
+            { name: "Chicken", quantity: 6, price: 2 },
+            { name: "Fries", quantity: 3, price: 3 },
+            { name: "Sushi", quantity: 2, price: 2 },
+            { name: "Steak", quantity: 1, price: 20 },
+            { name: "Pork", quantity: 1, price: 10 },
+            { name: "Bacon",  quantity: 5, price: 3}
         ];
-        
-        // declare already bought "AB"
+        // declare already bought "AB" array
         service.alreadyBought = [];
-
         // return array to ng-repeat
         service.getTB = function () {
             return service.toBuy;
@@ -54,27 +51,23 @@
         service.emptyTB = function () {
             return service.toBuy.length == 0;
         };
-
         // JS splice from array and push to array function
         service.moveItem = function (index) {
             // splice item at index
             var item = service.toBuy.splice(index, 1)[0];
+            // push to AB array
             service.alreadyBought.push(item);
             // console.log(service.toBuy)
             // console.log(service.alreadyBought)
         };
-
-       
         // return array to ng-repeat
         service.getAB = function () {
             return service.alreadyBought;
         };
-
-        
         // return true to ng-if
         service.emptyAB = function () {
             return service.alreadyBought.length == 0;
         };
-    }
+    } // end service
 
 })();
