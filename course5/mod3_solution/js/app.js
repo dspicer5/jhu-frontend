5d3c50b7dc8b459b966dbca90c4e44afb8f3b829
dspicer5 <80365857+dspicer5@users.noreply.github.com>
2022-03-17 15:28:38 -0400

    all but jhu

---
+++ course5/mod3_solution/js/app.js
@@ -0,0 +1,88 @@
+(function () {
+    'use strict';
+    // begin module
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems',FoundItemsDirective);
+    
+    // controller
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var NDC = this;
+        NDC.found = [];  
+        NDC.Menu = function () {
+            // null search
+            if((NDC.searchTerm == undefined) ||(NDC.searchTerm == '')){
+                NDC.message = "Nothing found";
+                NDC.found = []; // empty array
+            } 
+            else {
+                var promise = MenuSearchService.getMenuItems(NDC.searchTerm);
+                // console.log(promise);  // testing
+                promise.then(function(response){
+                    NDC.found = response;
+                    //console.log(NDC.found); // testing 
+                    if(NDC.found.length < 1){
+                        NDC.message = "Nothing found";
+                    } // end if
+                    else {
+                        NDC.message = "";
+                    } // end else
+                }) // end fucntion
+            }; // end else     
+        } // end menu function
+        NDC.removeItem = function(itemIndex){
+            NDC.found.splice(itemIndex, 1);
+            if(NDC.found.length < 1){
+                NDC.message = "No more matching items";
+            } 
+        } // end splice
+    }// end function
+
+    // http service
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http) {
+        var service = this;
+        service.getMenuItems = function(searchTerm){
+            var found = [];
+            return $http({
+                method: "GET",
+                // url designated in assignment
+                url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+            })
+            .then(function (result) {
+                for(var i=0; i<result.data.menu_items.length; i++){
+                    var description = result.data.menu_items[i].description;
+                    // push mathed items to found
+                    if (description.toLowerCase().indexOf(searchTerm.toLowerCase())!==-1) {
+                        found.push(result.data.menu_items[i]);	
+                    } // end if 
+                } // end for loop
+               return found;
+            }); // end function
+        } // end fuction / service
+    } // end function / http service
+
+    // directive from lesson 30
+    function FoundItemsDirective() {
+        // define ddo
+        var ddo={
+            templateUrl: 'foundItems.html',
+            scope: {
+                found: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownDirectiveController,
+            controllerAs: 'list',
+            bindToController: true
+        };
+        return ddo;
+    } // end function
+    
+    // construct
+    function NarrowItDownDirectiveController() {
+        var list = this;
+    }
+
+}());
