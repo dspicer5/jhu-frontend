869b8e5a8bb18d56e929b4c46ea01fd651f678e9
dspicer5 <80365857+dspicer5@users.noreply.github.com>
2022-04-27 19:28:06 -0400

    please work

---
+++ course5/mod5_solution/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,76 @@
+(function () {
+    'use strict';
+
+    angular.module('public')
+        .controller('SignUpController', SignUpController);
+
+    SignUpController.$inject = ['$timeout', '$scope', 'MenuService', 'SignUpService'];
+    function SignUpController($timeout, $scope, MenuService, SignUpService) {
+        var $ctrl = this;
+        var service = SignUpService;
+        $ctrl.data = {};
+        $ctrl.verified = false;
+
+        $ctrl.signUp = function () {
+            $ctrl.data.firstName = $ctrl.capitalizeFirst($ctrl.data.firstName);
+            $ctrl.data.lastName = $ctrl.capitalizeFirst($ctrl.data.lastName);
+            $ctrl.data.email = $ctrl.lowerCase($ctrl.data.email);
+            $ctrl.data.phone = $ctrl.seperate($ctrl.data.phone);
+            $ctrl.data.favoriteDish = $ctrl.upperCase($ctrl.data.favoriteDish);
+            $ctrl.checkData();
+        }
+
+        $ctrl.checkData = function () {
+            $ctrl.verified = false;
+            $ctrl.enrolled = true;
+            var dish = $ctrl.data.favoriteDish;
+
+            if (!dish) {
+                service.setSignUpData($ctrl.data);
+                $ctrl.data = {};
+                $ctrl.data.message = 'Your information has been saved.';
+                $scope.signupForm.$setPristine();
+                $ctrl.verified = true;
+                return;
+            }
+
+            MenuService.checkDish(dish).then(function (valid) {
+                if (valid) {
+                    $timeout(function () {
+                        service.setSignUpData($ctrl.data);
+                        $ctrl.data = {};
+                        $ctrl.data.message = 'Your information has been saved.';
+                        $scope.signupForm.$setPristine();
+                        $ctrl.verified = true;
+                    });
+                }
+                else {
+                    $ctrl.data.message = "No such menu number exists.";
+                    $ctrl.enrolled = false;
+                    $ctrl.verified = true;
+                }
+            })
+        }
+
+        $ctrl.lowerCase = function (data) {
+                var lower = data.toLowerCase();
+                return lower;
+        }
+        $ctrl.upperCase = function (data) {
+            var upper = data.toUpperCase();
+            return upper;
+        }
+        $ctrl.capitalizeFirst = function (data) {
+            var cap = data.toLowerCase();
+            cap = cap.substring(0, 1).toUpperCase() + cap.substring(1);
+            return cap;
+        }
+        $ctrl.seperate = function (data) {
+            var area = data.substring(0, 3);
+            var mid = data.substring(3, 6);
+            var end = data.substring(6, 10);
+            var seperated = area + "-" + mid + "-" + end;
+            return seperated;
+        }
+    }
+})();
