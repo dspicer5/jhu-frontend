85ab86b814d8d4ddf9c1acee998e4cb1c73dfd74
dspicer5 <80365857+dspicer5@users.noreply.github.com>
2022-04-27 16:28:55 -0400

    good test

---
+++ course5/mod5_solution/src/public/sign-up/sign-up.service.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    angular.module('public')
+        .service('SignUpService', SignUpService);
+
+    SignUpService.$inject = ['MenuService'];
+    function SignUpService(MenuService) {
+        var service = this;
+        var UserInfo = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            favoriteDish: ''
+    
+        };
+
+        service.setSignUpData = function (data) {
+            UserInfo = data;
+
+            MenuService.getDish(data.favoriteDish)
+                .then(function (response) {
+                        UserInfo.dish = response.data;
+                    }
+                );
+        }
+
+        service.getSignUpData = function () {
+            return UserInfo;
+        }
+        // email as unique value
+        service.enrolled = function () {
+            return UserInfo.email;
+        }
+    }
+})();
